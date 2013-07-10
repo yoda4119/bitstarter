659ac557ab08b007c40b52e58e678ef983ec19b6
Ubuntu <ubuntu@ip-172-31-21-61.us-west-2.compute.internal>
2013-07-10 16:30:08 +0000

    Added web.js

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+	  var fs = require('fs');
   var buf = fs.readFileSync('index.html');
   buf.toString('utf-8');
   response.send(buf);
